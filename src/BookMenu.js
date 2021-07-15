36389acffb5a705ef368c54dbd561c7ff5e2d313
Jenny Aguilar <contact@jaguilarweb.com>
2021-07-15 11:15:12 -0400

    feat: Change menu options in raw form

--- src/BookMenu.js
+++ src/BookMenu.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react'
 
+//From here I can not keep the changes to see them on search page
 class BookMenu extends Component{
+  state = {
+    value: 'wantToRead'
+  }
+
+  handleChange = (event) => {
+    this.setState({value: event.target.value});
+  }
+
   render(){
+    
     return(
-      <select>
-          <option value="move" disabled>Move to...</option>
-          <option value="currentlyReading">Currently Reading</option>
-          <option value="wantToRead">Want to Read</option>
-          <option value="read">Read</option>
-          <option value="none">None</option>
-      </select>
+        <select value={this.state.value} onChange={this.handleChange}>
+            <option value="move" disabled>Move to...</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read" >Read</option>
+            <option value="none">None</option>
+        </select>
     )
   }
 }
