5b76a8de126f54fcc1f14f9ad920244496421cdc
Jenny Aguilar <contact@jaguilarweb.com>
2021-08-23 21:21:33 -0400

    refactor: Change changeShelfBook function to set the state properly

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class BooksApp extends React.Component {
   state = {
     loading: true,
     error: null,
-    books: undefined
+    books: []
   }
 
   //Call api to fill state
@@ -42,34 +42,26 @@ class BooksApp extends React.Component {
     return this.state.books.find(b => b.id === book.id)
   }
 
-  //https://knowledge.udacity.com/questions/490197
-  // I did not use the Response of Api update function
-    //Add new book in the list to be displayed
-  addBookToList = (book) => {
-    let newBooksAdded = this.state.books
-      newBooksAdded.push(book)
-      this.setState(() => ({
-      books: newBooksAdded
-    }))
-  }
-
   // Function change the book shelf
   changeShelfBook = (book, shelf) => {
-    let booksUpdated = []
-    if(this.isBookInState(book) === undefined){
-      this.addBookToList(book)
-    }
-    this.state.books.map((b) => {
-      if(b.id === book.id) {
-        BooksAPI.update(b, shelf)
-        b.shelf = shelf
+    this.setState((currentState) => {
+      let booksUpdated = []
+      let newBookState = []
+      if (this.isBookInState(book) === undefined){
+        booksUpdated = [...currentState.books, book]
+      }else{
+        booksUpdated = currentState.books
       }
-      booksUpdated.push(b)
-      return booksUpdated
+      booksUpdated.map((b) => {
+          if(b.id === book.id) {
+            BooksAPI.update(b, shelf)
+            b.shelf = shelf
+          }
+          newBookState.push(b)
+          return newBookState
+        })
+      return {books: newBookState}
     })
-    this.setState(() => ({
-      books: booksUpdated
-    }))
   }
 
   render() {
--- src/Book.js
+++ src/Book.js
@@ -17,7 +17,7 @@ class Book extends Component {
             <li key = {book.id}>
               <div className="book">
                 <div className="book-top">
-                {book.imageLinks.smallThumbnail && (
+                {book.imageLinks.smallThumbnail !== undefined && (
                   <div className="book-cover" 
                     style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})`}}>
                   </div>)}
